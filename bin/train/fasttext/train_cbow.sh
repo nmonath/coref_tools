a5f9578500f108f2bb202d853110f5921cf52a3c
Nicholas Monath <nicholas.monath@gmail.com>
2018-09-01 10:26:51 -0400

    fix

--- bin/train/fasttext/train_cbow.sh
+++ bin/train/fasttext/train_cbow.sh
@@ -23,4 +23,4 @@ sbatch -J ft-${corpus_name} \
             --nodes=1 \
             --mem-per-cpu=6000 \
             --time=0-04:00 \
-            --mail-user $USER@cs.umass.edu --mail-type=$EMAIL fasttext skipgram -input $input -output exp_out/$corpus_name/$TIME -minn 2 -maxn 5 -dim 100 -ws 3
+            --mail-user $USER@cs.umass.edu --mail-type=$EMAIL sh bin/train/fasttext/train_cbow_.sh $input exp_out/$corpus_name/$TIME
---
+++ bin/train/fasttext/train_cbow_.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -exu
+
+input=$1
+output=$2
+
+
+fasttext cbow -input $input -output $output -minn 2 -maxn 5 -dim 100 -ws
