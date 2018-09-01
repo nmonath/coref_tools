4952c448d5fd8ce0b1102bf9c598e41db401e274
Nicholas Monath <nicholas.monath@gmail.com>
2018-08-31 23:27:01 -0400

    train cbow

---
+++ bin/train/fasttext/train_cbow.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+input=$1
+corpus_name=$2
+threads=$3
+TIME=`(date +%Y-%m-%d-%H-%M-%S)`
+EMAIL=${4:-None}
+
+export MKL_NUM_THREADS=$threads
+export OPENBLAS_NUM_THREADS=$threads
+export OMP_NUM_THREADS=$threads
+
+mkdir -p logs/train/ft/$corpus_name
+
+sbatch -J pw-tr${i}-${corpus_name} \
+            -e logs/train/ft/$corpus_name/${TIME}/${corpus_name}.err \
+            -o logs/train/ft/$corpus_name/${TIME}/${corpus_name}.log \
+            --cpus-per-task $threads \
+            --partition=defq \
+            --ntasks=1 \
+            --nodes=1 \
+            --mem-per-cpu=6000 \
+            --time=0-04:00 \
+            --mail-user $USER@cs.umass.edu --mail-type=$EMAIL fasttext skipgram -input $input -output exp_out/$corpus_name/$TIME -minn 2 -maxn 5 -dim 100 -ws 3
