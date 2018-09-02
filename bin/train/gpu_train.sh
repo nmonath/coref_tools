051b48ff87b6fea7fb8dc0ff0395a57825f8f1c0
Nicholas Monath <nicholas.monath@gmail.com>
2018-09-02 12:50:37 -0400

    train

--- bin/train/gpu_train.sh
+++ bin/train/gpu_train.sh
@@ -7,22 +7,28 @@ threads=1
 partition=${2:-titanx-short}
 EMAIL=${3:-None}
 
+dataset_name=`cat $CONFIG | jq -r .dataset_name`
+model_name=`cat $CONFIG | jq -r .model_name`
+alg_name=`cat $CONFIG | jq -r .alg_name`
+
 TIME=`(date +%Y-%m-%d-%H-%M-%S)`
+
+log_dir=logs/train/$dataset_name/$model_name/$alg_name/$TIME
 
 export MKL_NUM_THREADS=$threads
 export OPENBLAS_NUM_THREADS=$threads
 export OMP_NUM_THREADS=$threads
 
-mkdir -p logs/train/sample_pairs/${TIME}/
+mkdir -p $log_dir/
 
 sbatch -J sample_pairs \
-            -e logs/train/sample_pairs/${TIME}/sample_pairs.err \
-            -o logs/train/sample_pairs/${TIME}/sample_pairs.log \
+            -e $log_dir/sample_pairs.err \
+            -o $log_dir/sample_pairs.log \
             --cpus-per-task $threads \
             --partition=$partition \
             --gres=gpu:1 \
             --ntasks=1 \
             --nodes=1 \
-            --mem-per-cpu=6000 \
+            --mem-per-cpu=12000 \
             --time=0-04:00 \
             --mail-user $USER@cs.umass.edu --mail-type=$EMAIL bin/train/train.sh $input
