33779d4305532c1616dd78de77688d1db050d8d4
Nicholas Monath <nicholas.monath@gmail.com>
2018-09-03 11:05:07 -0400

    convert to set

--- src/python/grinch/models/nn/NSW.py
+++ src/python/grinch/models/nn/NSW.py
@@ -77,7 +77,7 @@ class NSW(object):
         allowable = nodes.difference(offlimits)
 
         if self.use_canopies:
-            offlimits = offlimits.union(self.nodes.difference(allowable))
+            offlimits = set(offlimits).union(self.nodes.difference(allowable))
 
         if len(allowable) == 0 \
                 or k * self.r * np.log(len(allowable)) > len(allowable) or self.exact_nn:
